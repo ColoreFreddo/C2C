df2be480ec9bd9d082aa6cbd5738a5c7764ebeb7
Daniel Viola <daniel.viola.apple@icloud.com>
2024-06-21 17:20:11 +0200

    sis

---
+++ client.py
@@ -0,0 +1,22 @@
+import socket
+
+HOST = 'localhost'  # The server's hostname or IP address
+PORT = 5000        # The port used by the server
+
+def main():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((HOST, PORT))
+
+    print(f"Connected to server on port {PORT}")
+
+    while True:
+        message = input("")
+        if message:
+            client_socket.sendall(message.encode('utf-8'))
+        else:
+            break
+
+    client_socket.close()
+
+if __name__ == "__main__":
+    main()
