79d6b06daed46c97cd5ee72a3b75283db5f5b8bd
Daniel Viola <daniel.viola.apple@icloud.com>
2024-06-21 20:09:16 +0200

    peer to peer implementation

---
+++ connect.py
@@ -0,0 +1,26 @@
+import socket
+
+def get_remote_info():
+  HOST = input("Enter remote peer's IP address: ")
+  PORT = int(input("Enter remote peer's port: "))
+  HOST, PORT = get_remote_info()
+  client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  client_socket.connect((HOST, PORT))
+
+  print(f"Connected to peer on {HOST}:{PORT}")
+
+  while True:
+    message = input("")
+    if message:
+      client_socket.sendall(message.encode('utf-8'))
+    else:
+      break
+    
+    data = client_socket.recv(1024).decode('utf-8')
+    if data:
+      print(f"Received: {data}")
+    else:
+      print("Peer disconnected.")
+      break
+
+  client_socket.close()
